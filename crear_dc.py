4b67d07685f415339c6819be2ad3a73bea2151e9
Juan Martin Nazarian <jmnazarian@gmail.com>
2026-09-08 18:28:02 -0300

    add port

--- crear_dc.py
+++ crear_dc.py
@@ -14,6 +14,8 @@ try:
       context: ./services/server
       dockerfile: Dockerfile
     container_name: server
+    ports:
+      - 5678:5678
     environment:
       - PYTHONUNBUFFERED=1
       - SERVER_HOST=server
